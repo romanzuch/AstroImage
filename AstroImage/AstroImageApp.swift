b3a73c7bd64d86c631ee746ac7b56e26f571f681
Roman <roman.zuchowski@gmail.com>
2022-09-13 18:25:11 +0200

    Initial Commit

---
+++ AstroImage/AstroImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroImageApp.swift
+//  AstroImage
+//
+//  Created by Roman on 13.09.22.
+//
+
+import SwiftUI
+
+@main
+struct AstroImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
