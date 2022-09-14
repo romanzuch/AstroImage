37b613301a044bbd9926d07023110da5b0a2ad3f
Roman <roman.zuchowski@gmail.com>
2022-09-14 18:00:30 +0200

    Added the main views

---
+++ AstroImage/View/More/MoreView.swift
@@ -0,0 +1,20 @@
+//
+//  MoreView.swift
+//  AstroImage
+//
+//  Created by Roman on 14.09.22.
+//
+
+import SwiftUI
+
+struct MoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreView()
+    }
+}
