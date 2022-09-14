615897f57e65656e99ea888e4770262d6546639d
Roman <roman.zuchowski@gmail.com>
2022-09-14 18:58:45 +0200

    Basic view structure

--- AstroImage/View/More/MoreView.swift
+++ AstroImage/View/More/MoreView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct MoreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Form {
+                    // here come the settings
+                }
+                List {
+                    // here comes something more
+                }
+            }
+            .navigationTitle("More")
+        }
     }
 }
 
