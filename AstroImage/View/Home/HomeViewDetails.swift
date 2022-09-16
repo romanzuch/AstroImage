000965fe425701f2b0a1a8627e2488d5d9956b6c
Roman <roman.zuchowski@gmail.com>
2022-09-16 20:30:16 +0200

    Refactored code

---
+++ AstroImage/View/Home/HomeViewDetails.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewDetails.swift
+//  AstroImage
+//
+//  Created by Roman on 16.09.22.
+//
+
+import SwiftUI
+
+struct HomeViewDetails: View {
+    
+    let response: APODResponse
+    
+    init(for response: APODResponse) {
+        self.response = response
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(response.date)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            Text(response.title)
+                .font(.title)
+                .foregroundColor(.white)
+            Text(response.explanation)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct HomeViewDetails_Previews: PreviewProvider {
+    static let homeVM: HomeViewModel = HomeViewModel()
+    static var previews: some View {
+        ZStack {
+            Color.gray
+                .ignoresSafeArea()
+            HomeViewDetails(for: homeVM.testData)
+        }
+    }
+}
