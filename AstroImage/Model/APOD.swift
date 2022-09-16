ffbbf2ecd9488e9791a63b4cfb7b0ba45faa2f8f
Roman <roman.zuchowski@gmail.com>
2022-09-16 19:53:10 +0200

    data model completed

--- AstroImage/Model/APOD.swift
+++ AstroImage/Model/APOD.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 struct APODResponse: Codable {
-    var author: String?
-    var date: String
-    var explanation: String
-    var hdurl: String?
-    var sdurl: String
-    var type: String
-    var title: String
+    var author: String? = ""
+    var date: String = ""
+    var explanation: String = ""
+    var hdurl: String? = ""
+    var sdurl: String = ""
+    var type: String = ""
+    var title: String = ""
     
     enum CodingKeys: String, CodingKey {
         case author = "copyright"
@@ -22,6 +22,8 @@ struct APODResponse: Codable {
         case type = "media_type"
         case date, explanation, hdurl, title
     }
+    
+    init() {}
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -32,6 +34,25 @@ struct APODResponse: Codable {
         sdurl = try container.decode(String.self, forKey: .sdurl)
         type = try container.decode(String.self, forKey: .type)
         title = try container.decode(String.self, forKey: .title)
-    }	
+    }
+    
+    //MARK: - Test Data
+    init(
+        author: String,
+        date: String,
+        explanation: String,
+        hdurl: String,
+        sdurl: String,
+        type: String,
+        title: String
+    ) {
+        self.author = author
+        self.date = date
+        self.explanation = explanation
+        self.hdurl = hdurl
+        self.sdurl = sdurl
+        self.type = type
+        self.title = title
+    }
     
 }
