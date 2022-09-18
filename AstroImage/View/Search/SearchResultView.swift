746fd06edf7de5c3d69c3dd525cfbc861387a207
Roman <roman.zuchowski@gmail.com>
2022-09-18 13:00:09 +0200

    Implemented new detail view link

--- AstroImage/View/Search/SearchResultView.swift
+++ AstroImage/View/Search/SearchResultView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct SearchResultView: View {
     
+    var result: APODResponse
     var title: String
     var date: String
     var imageUrl: URL
     var hdImageUrl: URL
     let formatter: DateFormatter = DateFormatter()
     
+    //MARK: - View Model
+    let searchResultVM: SearchResultViewModel = SearchResultViewModel()
+    
     init(for result: APODResponse) {
+        self.result = result
         self.formatter.dateFormat = "yyyy-MM-dd"
         self.title = result.title
         let rawDate = formatter.date(from: result.date)!
@@ -27,13 +32,16 @@ struct SearchResultView: View {
     
     var body: some View {
         NavigationLink {
-            Text("Details")
+            SearchDetailView(for: result)
         } label: {
-            HStack(alignment: .top) {
-                // image
+            HStack {
+                APODImage(for: result, type: .full)
+                    .frame(width: 80, height: 80)
+                    .cornerRadius(8)
                 VStack(alignment: .leading) {
                     Text(date).fontWeight(.semibold)
                     Text(title).fontWeight(.light)
+                        .multilineTextAlignment(.leading)
                 }
             }
         }
