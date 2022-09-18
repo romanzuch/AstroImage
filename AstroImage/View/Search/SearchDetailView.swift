76080cb8617f036bf4b63894106ab65bc1ea61d7
Roman <roman.zuchowski@gmail.com>
2022-09-18 13:22:30 +0200

    Showing the image details like in the home view

--- AstroImage/View/Search/SearchDetailView.swift
+++ AstroImage/View/Search/SearchDetailView.swift
@@ -10,8 +10,16 @@ import SwiftUI
 struct SearchDetailView: View {
     
     var result: APODResponse
-    @State private var showInfo: Bool = false
     let searchDetailVM: SearchDetailViewModel = SearchDetailViewModel()
+    @State private var downloadInProgress: Bool = false
+    
+    //MARK: - Gesture properties
+    @State private var translation: CGFloat = 0
+    @State private var lastTranslation: CGFloat = 0
+    @GestureState private var gestureTranslation: CGSize = .zero
+    
+    //MARK: - Detail View Properties
+    @State private var detailsExtended: Bool = false
     
     init(for result: APODResponse) {
         self.result = result
@@ -25,27 +33,72 @@ struct SearchDetailView: View {
                     .frame(width: frame.width, height: frame.height)
             }
             .ignoresSafeArea()
-            //MARK: - Foreground info screen
-            Group {
-                if showInfo {
-                    details
-                }
+            //MARK: - Bottom sheet for details
+            GeometryReader { geo in
+                //TODO: - The max offset of the extended detail view has
+                //TODO: - to be changed to take the container's size
+                //TODO: - into account.
+                let height = geo.frame(in: .global).height
+                let width = geo.frame(in: .global).width
+                let containerHeight = geo.frame(in: .local).height
+                let maxTranslationValue = -containerHeight + 240
+                details
+                    .background {
+                        Rectangle()
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 3)
+                            .offset(y: height + 320) // tab bar height 83px
+                            .foregroundColor(.black)
+                            .opacity(translation == 0 ? 0 : 0.3)
+                    }
+                    .padding(.horizontal)
+                    .frame(width: width, height: height)
+                    .offset(y: height - 180)
+                    .offset(y: translation)
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                translation = value.translation.height + lastTranslation
+                            })
+                            .onEnded({ value in
+                                if value.translation.height < 0 {
+                                    withAnimation {
+                                        translation = maxTranslationValue
+                                        detailsExtended = true
+                                    }
+                                } else {
+                                    withAnimation {
+                                        translation = 0
+                                        detailsExtended = false
+                                    }
+                                }
+                                lastTranslation = translation
+                            })
+                    )
             }
         }.toolbar {
             ToolbarItemGroup(placement: .navigationBarTrailing) {
                 Button {
-                    showInfo.toggle()
-                } label: {
-                    Image(systemName: showInfo ? "info.circle.fill" : "info.circle")
-                }
-                Button {
                     Task {
-                        guard let image = await searchDetailVM.getImageDownload(for: result) else { return }
+                        guard let image = await searchDetailVM.getImageDownload(for: result, inProgress: $downloadInProgress) else { return }
                         let imageSaver = ImageSaver()
                         imageSaver.writeToPhotoAlbum(image: image)
                     }
                 } label: {
-                    Image(systemName: "arrow.down.circle")
+                    Group {
+                        if !downloadInProgress {
+                            Image(systemName: "arrow.down.circle")
+                        } else {
+                            ProgressView()
+                                .progressViewStyle(.circular)
+                                .tint(.accentColor)
+                        }
+                    }
+                }
+                
+                Button {
+                    debugPrint("Share")
+                } label: {
+                    Image(systemName: "square.and.arrow.up")
                 }
             }
         }
@@ -53,19 +106,13 @@ struct SearchDetailView: View {
     
     var details: some View {
         VStack {
-            Text(result.title)
-                .font(.title)
-                .padding(.bottom)
-            ScrollView(.vertical, showsIndicators: false) {
-                Text(result.explanation)
-                    .multilineTextAlignment(.leading)
-                    .padding(.horizontal)
-            }
+            ImageDetails(
+                for: result,
+                extended: $detailsExtended,
+                translation: $translation
+            )
+            Spacer()
         }
-        .padding()
-        .frame(width: UIScreen.main.bounds.width - 64)
-        .background(.ultraThinMaterial)
-        .cornerRadius(12)
     }
     
 }
