09c6a2b3d0188e2e28eeea2b538e6cbac82205a4
Roman <roman.zuchowski@gmail.com>
2022-09-18 10:27:01 +0200

    Added method to catch results

--- AstroImage/ViewModel/Search/SearchVM.swift
+++ AstroImage/ViewModel/Search/SearchVM.swift
@@ -13,4 +13,17 @@ class SearchViewModel: ObservableObject {
     @Published var startDate: Date = Date()
     @Published var endDate: Date = Date()
     
+    // MARK: - Response data
+    @Published var data: [APODResponse]?
+    
+    //MARK: - API Methods relevant to the Home View
+    let apodApi: APODApi = APODApi.singleton
+    func getImagesFromDateRange() async {
+        await apodApi.getImagesFromDateRange(from: startDate, to: endDate) { apiResponse in
+            DispatchQueue.main.async {
+                self.data = apiResponse
+            }
+        }
+    }
+    
 }
