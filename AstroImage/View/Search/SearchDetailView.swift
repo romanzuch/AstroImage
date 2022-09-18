0acd4ec6f94a9ae7322448b8d2690b77c24f9d8f
Roman <roman.zuchowski@gmail.com>
2022-09-18 12:59:58 +0200

    Added new download button

--- AstroImage/View/Search/SearchDetailView.swift
+++ AstroImage/View/Search/SearchDetailView.swift
@@ -11,6 +11,7 @@ struct SearchDetailView: View {
     
     var result: APODResponse
     @State private var showInfo: Bool = false
+    let searchDetailVM: SearchDetailViewModel = SearchDetailViewModel()
     
     init(for result: APODResponse) {
         self.result = result
@@ -31,11 +32,20 @@ struct SearchDetailView: View {
                 }
             }
         }.toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
                 Button {
                     showInfo.toggle()
                 } label: {
                     Image(systemName: showInfo ? "info.circle.fill" : "info.circle")
+                }
+                Button {
+                    Task {
+                        guard let image = await searchDetailVM.getImageDownload(for: result) else { return }
+                        let imageSaver = ImageSaver()
+                        imageSaver.writeToPhotoAlbum(image: image)
+                    }
+                } label: {
+                    Image(systemName: "arrow.down.circle")
                 }
             }
         }
